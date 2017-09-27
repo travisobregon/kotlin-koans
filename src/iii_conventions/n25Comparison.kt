abf42fae08a6193e1260fd12081162f82afd55d2
Travis Obregon <travis.obregon@ericsson.com>
2017-09-27 12:51:32 +0100

    :white_check_mark: Comparison

--- src/iii_conventions/n25Comparison.kt
+++ src/iii_conventions/n25Comparison.kt
@@ -15,8 +15,4 @@ fun todoTask25(): Nothing = TODO(
     references = { date: MyDate, comparable: Comparable<MyDate> -> }
 )
 
-fun task25(date1: MyDate, date2: MyDate): Boolean {
-    todoTask25()
-//    return date1 < date2
-}
-
+fun task25(date1: MyDate, date2: MyDate) = date1 < date2
