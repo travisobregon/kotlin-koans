215d3d2cd6675fec7b9cacea5fc6ebfd314614c1
Travis Obregon <travis.obregon@ericsson.com>
2017-09-27 15:24:36 +0100

    :white_check_mark: Operators Overloading

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -10,11 +10,19 @@ data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparab
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
 
+operator fun MyDate.plus(timeInterval: TimeInterval) : MyDate = this.addTimeIntervals(timeInterval, 1)
+
+operator fun MyDate.plus(repeatedTimeInterval: RepeatedTimeInterval) : MyDate = this.addTimeIntervals(repeatedTimeInterval.ti, repeatedTimeInterval.n)
+
+class RepeatedTimeInterval(val ti: TimeInterval, val n: Int)
+
 enum class TimeInterval {
     DAY,
     WEEK,
     YEAR
 }
+
+operator fun TimeInterval.times(n: Int) : RepeatedTimeInterval = RepeatedTimeInterval(this, n)
 
 class DateRange(override val start: MyDate, override val endInclusive: MyDate): ClosedRange<MyDate>, Iterable<MyDate> {
     override fun iterator() = object : Iterator<MyDate> {
