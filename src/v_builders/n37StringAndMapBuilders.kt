3f673fb31ce7f43a7cc8a88c0f281499120bad4d
Travis Obregon <travis.obregon@ericsson.com>
2017-09-28 11:09:44 +0100

    :white_check_mark: String And Map Builders

--- src/v_builders/n37StringAndMapBuilders.kt
+++ src/v_builders/n37StringAndMapBuilders.kt
@@ -2,6 +2,7 @@ package v_builders
 
 import util.TODO
 import java.util.*
+import kotlin.collections.HashMap
 
 fun buildStringExample(): String {
     fun buildString(build: StringBuilder.() -> Unit): String {
@@ -29,11 +30,16 @@ fun todoTask37(): Nothing = TODO(
 )
 
 fun task37(): Map<Int, String> {
-    todoTask37()
-//    return buildMap {
-//        put(0, "0")
-//        for (i in 1..10) {
-//            put(i, "$i")
-//        }
-//    }
+    fun <K, V> buildMap(build: HashMap<K, V>.() -> Unit): Map<K, V> {
+        val map = HashMap<K, V>()
+        map.build()
+        return map
+    }
+
+    return buildMap {
+        put(0, "0")
+        for (i in 1..10) {
+            put(i, "$i")
+        }
+    }
 }
